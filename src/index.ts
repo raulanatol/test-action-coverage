213bb7b40f4c74cb279592ce75d2cf13155c01bf
raulanatol <raul@natol.es>
2020-12-30 15:02:23 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { getHelloMessage } from "./utils";
+
+const app = express();
+
+export const sayHello = (req, res) => {
+  res.status(200).json({
+    message: getHelloMessage(),
+    uptime: process.uptime()
+  });
+};
+
+app.use("/", sayHello);
+
+const port = 3001;
+app.listen(port, () => {
+  console.log("🚀 Running at localhost:3001");
+});
